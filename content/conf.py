574b020a56c3089de1776701d90b67fd2bf5c24c
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2022-03-02 13:29:53 +0100

    add css

--- content/conf.py
+++ content/conf.py
@@ -79,7 +79,7 @@ html_title = project
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ["css"]
 
 # HTML context:
 from os.path import dirname, realpath, basename
