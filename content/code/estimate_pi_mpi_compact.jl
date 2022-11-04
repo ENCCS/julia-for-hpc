87353c72b3dbb7c20409104befd001805883764b
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2022-11-04 12:03:44 +0100

    rm git conflict markers from MPI examples

--- content/code/estimate_pi_mpi_compact.jl
+++ content/code/estimate_pi_mpi_compact.jl
@@ -18,10 +18,7 @@ function estimate_pi(num_points)
 end
 
 function main()
-<<<<<<< HEAD
     t1 = time()
-=======
->>>>>>> main
     num_points = 10^9
 
     # divide work evenly between ranks
--- content/code/estimate_pi_mpi_general.jl
+++ content/code/estimate_pi_mpi_general.jl
@@ -18,10 +18,7 @@ function estimate_pi(num_points)
 end
 
 function main()
-<<<<<<< HEAD
     t1 = time()
-=======
->>>>>>> main
     num_points = 10^9
     num_jobs = 10
     chunks = [num_points / num_jobs for i in 1:num_jobs]
