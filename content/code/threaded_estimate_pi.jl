bca94f718a5d33ae4291ba0662e3a61cee67ca65
Jaan Tollander de Balsch <jtollan@dalripa.lnx.csc.fi>
2023-10-09 12:05:19 +0300

    fixed code

--- content/code/threaded_estimate_pi.jl
+++ content/code/threaded_estimate_pi.jl
@@ -2,9 +2,11 @@ using Base.Threads
 
 function partial_hits(num_points)
     hits = zero(Int)
-    x, y = rand(), rand()
-    if x^2 + y^2 < 1.0
-        hits += 1
+    for _ in 1:num_points
+        x, y = rand(), rand()
+        if x^2 + y^2 < 1.0
+            hits += 1
+        end
     end
     return hits
 end
@@ -12,7 +14,7 @@ end
 function threaded_estimate_pi(num_points)
     n = nthreads()
     d, r = divrem(num_points, n)
-    chuncks = vcat(fill(d, n-r), fill(d+1, r))
+    chunks = vcat(fill(d, n-r), fill(d+1, r))
     tasks = map(chunks) do chunk
         @spawn partial_hits(chunk)
     end
