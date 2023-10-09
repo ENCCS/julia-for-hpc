d998d3e23ea6e0486d5e8e737f49da0b27cd8736
Jaan Tollander de Balsch <jtollan@dalripa.lnx.csc.fi>
2023-10-09 11:55:01 +0300

    fixed threaded estimate pi

--- content/code/threaded_estimate_pi.jl
+++ content/code/threaded_estimate_pi.jl
@@ -1,17 +1,21 @@
-using .Threads
+using Base.Threads
+
+function partial_hits(num_points)
+    hits = zero(Int)
+    x, y = rand(), rand()
+    if x^2 + y^2 < 1.0
+        hits += 1
+    end
+    return hits
+end
 
 function threaded_estimate_pi(num_points)
-    partial_hits = zeros(Int, nthreads())
-
-    @threads for _ in 1:num_points
-        x, y = rand(), rand()
-	if x^2 + y^2 < 1.0
-            partial_hits[threadid()] += 1
-	end
+    n = nthreads()
+    d, r = divrem(num_points, n)
+    chuncks = vcat(fill(d, n-r), fill(d+1, r))
+    tasks = map(chunks) do chunk
+        @spawn partial_hits(chunk)
     end
-
-    hits = sum(partial_hits)
-
-    fraction = hits / num_points
-    return 4 * fraction
+    hits = mapreduce(fetch, +, tasks; init=zero(Int))
+    return 4 * hits / num_points
 end
