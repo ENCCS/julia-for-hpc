bf9ca9421ef16e4b463f67e62244a7bbe1b52606
Jaan Tollander de Balsch <jtollan@dalripa.lnx.csc.fi>
2023-10-11 14:17:06 +0300

    fixed random number generation on multithreading

--- content/code/threaded_estimate_pi.jl
+++ content/code/threaded_estimate_pi.jl
@@ -1,9 +1,10 @@
+using Random
 using Base.Threads
 
-function partial_hits(num_points)
+function partial_hits(rng, num_points)
     hits = zero(Int)
     for _ in 1:num_points
-        x, y = rand(), rand()
+        x, y = rand(rng), rand(rng)
         if x^2 + y^2 < 1.0
             hits += 1
         end
@@ -13,10 +14,11 @@ end
 
 function threaded_estimate_pi(num_points)
     n = nthreads()
+    rngs = [MersenneTwister() for _ in 1:n]
     d, r = divrem(num_points, n)
-    chunks = vcat(fill(d, n-r), fill(d+1, r))
-    tasks = map(chunks) do chunk
-        @spawn partial_hits(chunk)
+    chunks = zip(rngs, vcat(fill(d, n-r), fill(d+1, r)))
+    tasks = map(chunks) do (rng, chunk)
+        @spawn partial_hits(rng, chunk)
     end
     hits = mapreduce(fetch, +, tasks; init=zero(Int))
     return 4 * hits / num_points
